8da4a0f16ae7391a8dc7295106ebae77f3625e98
oneboi42 <oneboiwork@gmail.com>
2026-05-28 11:28:07 +0200

    feat(ticket): add UpdatedAtUtc field and database migration

--- src/HelpDeskHero.Api/obj/Debug/net10.0/HelpDeskHero.Api.AssemblyInfo.cs
+++ src/HelpDeskHero.Api/obj/Debug/net10.0/HelpDeskHero.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelpDeskHero.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+190f71594a2dc7d85b2e21c26978ef5b87ef7ebb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e0d8d4e7faa99fb4f30873407535da711bedbd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelpDeskHero.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelpDeskHero.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
