dcb1aed09cc24d2a3b62e890fdf1384fec7f3448
oneboi42 <oneboiwork@gmail.com>
2026-05-29 18:50:42 +0200

    feat(UI): logout and menu

--- src/HelpDeskHero.UI/obj/Debug/net10.0/HelpDeskHero.UI.AssemblyInfo.cs
+++ src/HelpDeskHero.UI/obj/Debug/net10.0/HelpDeskHero.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelpDeskHero.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+190f71594a2dc7d85b2e21c26978ef5b87ef7ebb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff81707097751363eb5a30bb7251b519913faae6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelpDeskHero.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelpDeskHero.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
