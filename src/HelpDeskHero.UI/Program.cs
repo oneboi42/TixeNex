99221ae063f3d96ee8f5de11bbbb40081090d48f
oneboi42 <oneboiwork@gmail.com>
2026-05-29 18:21:40 +0200

    feat(ui): configure authentication in Program.cs

--- src/HelpDeskHero.UI/Program.cs
+++ src/HelpDeskHero.UI/Program.cs
@@ -1,5 +1,7 @@
 using HelpDeskHero.UI;
 using HelpDeskHero.UI.Services.Api;
+using HelpDeskHero.UI.Services.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,11 +13,23 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 var apiBaseUrl = builder.Configuration["Api:BaseUrl"]
     ?? throw new InvalidOperationException("Missing Api:BaseUrl.");
 
-builder.Services.AddScoped(_ => new HttpClient
+builder.Services.AddScoped<TokenStorageService>();
+builder.Services.AddScoped<AuthTokenHandler>();
+builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped(sp =>
 {
-    BaseAddress = new Uri(apiBaseUrl)
+    var handler = sp.GetRequiredService<AuthTokenHandler>();
+    handler.InnerHandler = new HttpClientHandler();
+
+    return new HttpClient(handler)
+    {
+        BaseAddress = new Uri(apiBaseUrl)
+    };
 });
 
 builder.Services.AddScoped<TicketApiClient>();
+builder.Services.AddScoped<AuthService>();
 
 await builder.Build().RunAsync();
