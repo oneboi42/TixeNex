200346e5630dbbc319d6151339909fd30256cdfd
oneboi42 <oneboiwork@gmail.com>
2026-06-04 19:00:28 +0200

    feat(API):Two new entities have been added: AppUser and RefreshToken

--- src/HelpDeskHero.Api/Infrastructure/Persistence/AppDbContext.cs
+++ src/HelpDeskHero.Api/Infrastructure/Persistence/AppDbContext.cs
@@ -10,6 +10,8 @@ public sealed class AppDbContext : DbContext
     }
 
     public DbSet<Ticket> Tickets => Set<Ticket>();
+    public DbSet<AppUser> Users => Set<AppUser>();
+    public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -19,14 +21,41 @@ public sealed class AppDbContext : DbContext
             b.HasKey(x => x.Id);
 
             b.Property(x => x.Number).HasMaxLength(30).IsRequired();
-            b.HasIndex(x => x.Number).IsUnique();
-
-            b.Property(x => x.Number).HasMaxLength(30).IsRequired();
             b.Property(x => x.Title).HasMaxLength(200).IsRequired();
             b.Property(x => x.Description).HasMaxLength(4000).IsRequired();
             b.Property(x => x.Status).HasMaxLength(30).IsRequired();
             b.Property(x => x.Priority).HasMaxLength(30).IsRequired();
             b.Property(x => x.CreatedAtUtc).IsRequired();
+        });
+
+        modelBuilder.Entity<AppUser>(b =>
+        {
+            b.ToTable("Users");
+            b.HasKey(x => x.Id);
+
+            b.Property(x => x.UserName).HasMaxLength(100).IsRequired();
+            b.HasIndex(x => x.UserName).IsUnique();
+
+            b.Property(x => x.PasswordHash).HasMaxLength(500).IsRequired();
+            b.Property(x => x.Role).HasMaxLength(50).IsRequired();
+        });
+
+        modelBuilder.Entity<RefreshToken>(b =>
+        {
+            b.ToTable("RefreshTokens");
+            b.HasKey(x => x.Id);
+
+            b.Property(x => x.Token).HasMaxLength(200).IsRequired();
+            b.HasIndex(x => x.Token).IsUnique();
+
+            b.Property(x => x.CreatedAtUtc).IsRequired();
+            b.Property(x => x.ExpiresAtUtc).IsRequired();
+            b.Property(x => x.ReplacedByToken).HasMaxLength(200);
+
+            b.HasOne(x => x.AppUser)
+             .WithMany(x => x.RefreshTokens)
+             .HasForeignKey(x => x.AppUserId)
+             .OnDelete(DeleteBehavior.Cascade);
         });
     }
 }
