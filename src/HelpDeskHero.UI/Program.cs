1cda2e086678bb9f2bc726f30792110094dc89bd
oneboi42 <oneboiwork@gmail.com>
2026-06-08 12:08:44 +0200

    feat(ui): DI registration

--- src/HelpDeskHero.UI/Program.cs
+++ src/HelpDeskHero.UI/Program.cs
@@ -1,5 +1,7 @@
 using HelpDeskHero.UI;
 using HelpDeskHero.UI.Services.Api;
+using Blazored.LocalStorage;
+using HelpDeskHero.UI.Handlers;
 using HelpDeskHero.UI.Services.Auth;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
@@ -10,17 +12,27 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped<TokenStorageService>();
+builder.Services.AddScoped<SessionTokenStore>();
+builder.Services.AddScoped<AuthSessionService>();
+
+builder.Services.AddScoped<JwtAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(sp =>
+    sp.GetRequiredService<JwtAuthenticationStateProvider>());
+
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AuthTokenHandler>();
+builder.Services.AddScoped<AuthHttpMessageHandler>();
+
 var apiBaseUrl = builder.Configuration["Api:BaseUrl"]
     ?? throw new InvalidOperationException("Missing Api:BaseUrl.");
 
-builder.Services.AddScoped<TokenStorageService>();
-builder.Services.AddScoped<AuthTokenHandler>();
-builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
-builder.Services.AddAuthorizationCore();
-
 builder.Services.AddScoped(sp =>
 {
-    var handler = sp.GetRequiredService<AuthTokenHandler>();
+    var handler = sp.GetRequiredService<AuthHttpMessageHandler>();
     handler.InnerHandler = new HttpClientHandler();
 
     return new HttpClient(handler)
@@ -29,6 +41,7 @@ builder.Services.AddScoped(sp =>
     };
 });
 
+builder.Services.AddScoped<AuthApiClient>();
 builder.Services.AddScoped<TicketApiClient>();
 builder.Services.AddScoped<AuthService>();
 
