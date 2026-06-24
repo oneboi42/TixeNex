77d558c855c59d626e79ca1647036582e2a05842
oneboi42 <oneboiwork@gmail.com>
2026-06-24 21:27:02 +0200

    test(api,ui): add API integration and bUnit tests for checkout feature

---
+++ tests/HelpDeskHero.Api.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace HelpDeskHero.Api.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/HelpDeskHero.UI.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace HelpDeskHero.UI.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
